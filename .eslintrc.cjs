c8e47b95a0e3102fdb52e9fbf3d075e82994cfb9
Moritz Tietze <59286548+ferb300@users.noreply.github.com>
2024-09-27 17:45:21 +0200

    linting and env setup

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,79 +6,99 @@
 
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
-  root: true,
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-    ecmaFeatures: {
-      jsx: true,
-    },
-  },
-  env: {
-    browser: true,
-    commonjs: true,
-    es6: true,
-  },
-  ignorePatterns: ["!**/.server", "!**/.client"],
-
-  // Base config
-  extends: ["eslint:recommended"],
-
-  overrides: [
-    // React
-    {
-      files: ["**/*.{js,jsx,ts,tsx}"],
-      plugins: ["react", "jsx-a11y"],
-      extends: [
-        "plugin:react/recommended",
-        "plugin:react/jsx-runtime",
-        "plugin:react-hooks/recommended",
-        "plugin:jsx-a11y/recommended",
-      ],
-      settings: {
-        react: {
-          version: "detect",
+    root: true,
+    parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        ecmaFeatures: {
+            jsx: true,
         },
-        formComponents: ["Form"],
-        linkComponents: [
-          { name: "Link", linkAttribute: "to" },
-          { name: "NavLink", linkAttribute: "to" },
-        ],
-        "import/resolver": {
-          typescript: {},
-        },
-      },
+    },
+    env: {
+        browser: true,
+        commonjs: true,
+        es6: true,
+    },
+    ignorePatterns: ["!**/.server", "!**/.client"],
+    rules: {
+        // Indent with 4 spaces
+        "indent": ["error", 4],
+
+        // Indent JSX with 4 spaces
+        "react/jsx-indent": ["error", 4],
+
+        // Indent props with 4 spaces
+        "react/jsx-indent-props": ["error", 4],
+
+        // force semicolon usage
+        "@stylistic/js/semi": 2,
+
+        // force double quotes
+        "@stylistic/js/quotes": 2,
     },
 
-    // Typescript
-    {
-      files: ["**/*.{ts,tsx}"],
-      plugins: ["@typescript-eslint", "import"],
-      parser: "@typescript-eslint/parser",
-      settings: {
-        "import/internal-regex": "^~/",
-        "import/resolver": {
-          node: {
-            extensions: [".ts", ".tsx"],
-          },
-          typescript: {
-            alwaysTryTypes: true,
-          },
-        },
-      },
-      extends: [
-        "plugin:@typescript-eslint/recommended",
-        "plugin:import/recommended",
-        "plugin:import/typescript",
-      ],
-    },
+    // Base config
+    extends: ["eslint:recommended"],
 
-    // Node
-    {
-      files: [".eslintrc.cjs"],
-      env: {
-        node: true,
-      },
-    },
-  ],
+    overrides: [
+        // React
+        {
+            files: ["**/*.{js,jsx,ts,tsx}"],
+            plugins: [
+                "react",
+                "jsx-a11y",
+                "@stylistic/js"
+            ],
+            extends: [
+                "plugin:react/recommended",
+                "plugin:react/jsx-runtime",
+                "plugin:react-hooks/recommended",
+                "plugin:jsx-a11y/recommended",
+            ],
+            settings: {
+                react: {
+                    version: "detect",
+                },
+                formComponents: ["Form"],
+                linkComponents: [
+                    { name: "Link", linkAttribute: "to" },
+                    { name: "NavLink", linkAttribute: "to" },
+                ],
+                "import/resolver": {
+                    typescript: {},
+                },
+            },
+        },
+
+        // Typescript
+        {
+            files: ["**/*.{ts,tsx}"],
+            plugins: ["@typescript-eslint", "import"],
+            parser: "@typescript-eslint/parser",
+            settings: {
+                "import/internal-regex": "^~/",
+                "import/resolver": {
+                    node: {
+                        extensions: [".ts", ".tsx"],
+                    },
+                    typescript: {
+                        alwaysTryTypes: true,
+                    },
+                },
+            },
+            extends: [
+                "plugin:@typescript-eslint/recommended",
+                "plugin:import/recommended",
+                "plugin:import/typescript",
+            ],
+        },
+
+        // Node
+        {
+            files: [".eslintrc.cjs"],
+            env: {
+                node: true,
+            },
+        },
+    ],
 };
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,15 +1,22 @@
-import type { MetaFunction } from "@remix-run/node";
+import { json, type LoaderFunction, type MetaFunction } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
 
 export const meta: MetaFunction = () => {
-  return [
-    { title: "smarties" }
-  ];
+    return [
+        { title: "smarties" }
+    ];
+};
+
+export const loader: LoaderFunction = async () => {
+    return json({ value: process.env.value });
 };
 
 export default function Index() {
-  return (
-    <p>
-      hi
-    </p>
-  );
+    const { value } = useLoaderData<typeof loader>();
+    return (
+        <p>
+            hi
+            {value}
+        </p>
+    );
 }
